4f0c47bc76fa15793bae09b7d5646b93b77b6af5
sudhakar <asudhakar@live.in>
2016-04-30 13:47:38 +0530

    final sms api

---
+++ view/dashboard.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome</title>
+
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <style type="text/css">
+
+  div>span{
+  	display: block;
+    font-size: 14px;
+    margin-bottom: 9px;
+  }
+
+  </style>
+</head>
+<body>
+<?php 
+
+	include_once '../app_functions/default_functions.php';
+
+	if(array_key_exists("GROUP BY",$final_output)){
+		unset($final_output['GROUP BY']);
+	}
+
+
+
+
+
+	
+ ?>
+<div class="container">
+
+
+
+<h1>Select Contacts</h1>
+
+<form method="post" action="message_process.php">
+
+<input type="hidden" name="path" value="<?php echo $file_path; ?>">
+<input type="checkbox" id="checkAll">Check All
+
+            
+  <table class="table">
+    <tbody>
+      <?php
+
+      $i = 1; 
+      $totalhtml = "";
+      foreach ($final_output as $class => $student_values) {
+      	$html1 = "<h2>$class</h2>";
+      	foreach ($student_values as $number_text => $number_and_names) {
+      		foreach ($number_and_names as $number => $names) {
+      			foreach ($names as $key => $name) {
+      				$html1 = $html1.'<span><input type="checkbox" name = "name[]" value="'.$number.'"> &nbsp;&nbsp;'.$name.'('.$number.')</span>';
+      			}
+      		}$totalhtml = $totalhtml.'<div>'.$html1.'</div><hr/>';
+      	}
+      } 
+
+      echo "$totalhtml";
+      $totalhtml = "";
+      $html1 = "";
+      
+      ?>
+    </tbody>
+  </table>
+  <input type="submit" class="btn btn-primary" value="submit">
+  <a href="../index.php"><input type="button" class="btn btn-warning" value="cancel"></a>
+
+  </form>
+</div>
+
+
+<script type="text/javascript">
+  
+
+$('body').on('change', '#checkAll', function(){
+      $("input:checkbox").prop('checked', $(this).prop("checked"));
+    });
+
+</script>
+
+
+
+</body>
+</html>
---
+++ view/insert_into_db.php
@@ -0,0 +1,38 @@
+<?php 
+
+include_once '../controllers/function.php';
+
+$name_and_numbers = $_POST['name_and_number'];
+$message = $_POST['message'];
+$name_and_numbers = unserialize(base64_decode($name_and_numbers));
+$path = $_POST['path'];
+
+
+
+
+
+$link = db_connect();
+
+foreach ($name_and_numbers as $key => $number) {
+	$sql = "INSERT INTO `MessageOut` (`MessageTo` , `MessageText`) VALUES ('$number', '$message')";
+	echo $sql;
+	executeQuery($sql, $link);
+	
+}
+header('Location: ../index.php');
+
+	function db_connect(){
+		$connection = mysqli_connect('gigabytehome.ddns.net', "demo", "demo1234", "demo", 3306);
+		if (!$connection) {
+		    die("Connection failed: " . mysqli_connect_error());
+		    exit();
+		}
+		return $connection;
+	}
+
+
+
+
+
+
+	
