57f52724b108b90054813b22131411f69d0c0471
sudhakar <asudhakar@live.in>
2016-04-30 14:52:06 +0530

    ui testing page

---
+++ ui_test.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Welcome</title>
+
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  </head>
+<body>
+
+	<fieldset>
+    <!-- these will be affected by check all -->
+    <div><input type="checkbox" class="checkall"> Check all</div>
+    <div><input type="checkbox"> Checkbox</div>
+    <div><input type="checkbox"> Checkbox</div>
+    <div><input type="checkbox"> Checkbox</div>
+</fieldset>
+<fieldset>
+    <!-- these won't be affected by check all; different field set -->
+    <div><input type="checkbox"> Checkbox</div>
+    <div><input type="checkbox"> Checkbox</div>
+    <div><input type="checkbox"> Checkbox</div>
+</fieldset>
+
+<script type="text/javascript">
+	
+
+	$(function () {
+    $('.checkall').on('click', function () {
+        $(this).closest('fieldset').find(':checkbox').prop('checked', this.checked);
+    });
+});
+</script>
+
+</body>
+</html>
