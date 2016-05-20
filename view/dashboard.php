8c8a67b4f7a48eca2502e7ebc90bc7537fa42328
sudhakar <asudhakar@live.in>
2016-05-20 08:12:27 +0530

    added br

--- view/dashboard.php
+++ view/dashboard.php
@@ -55,7 +55,7 @@
       		foreach ($number_and_names as $number => $names) {
       			foreach ($names as $key => $name) {
       				$html1 = $html1.'
-  <label><input type="checkbox" name = "name[]" value="'.$number.'"> &nbsp;&nbsp;'.$name.'('.$number.')</label>';
+  <label><input type="checkbox" name = "name[]" value="'.$number.'"> &nbsp;&nbsp;'.$name.'('.$number.')</label><br/>';
       			}
       		}$totalhtml = $totalhtml.'<div class="checkbox"><fieldset>'.$html1.'</fieldset></div><hr/>';
       	}
